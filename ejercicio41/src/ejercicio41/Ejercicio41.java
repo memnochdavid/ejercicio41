19f3c07d958ead7dd4f91bd53b213a28e32b637c
David Duque <davduquediaz@gmail.com>
2024-05-18 20:07:32 +0200

    cambios del alumno a

--- ejercicio41/src/ejercicio41/Ejercicio41.java
+++ ejercicio41/src/ejercicio41/Ejercicio41.java
@@ -2,6 +2,10 @@ package ejercicio41;
 public class Ejercicio41 {
     public static void main(String[] args) {
         System.out.println("Ejercicio 41");
+        System.out.println("Soy el alumno A.");
+        System.out.println("Estoy haciendo cambios.");
+        System.out.println("No hago ningún commit.");
+        System.out.println("Ya he terminado.");
     }
     
 }
