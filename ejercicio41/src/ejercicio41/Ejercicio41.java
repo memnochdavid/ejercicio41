c6a4a8d291b57b7e6d4103402bb51e89bd772e50
Dummy <dummy@dummy.com>
2024-05-18 20:03:27 +0200

    nuevas líneas del alumno b

--- ejercicio41/src/ejercicio41/Ejercicio41.java
+++ ejercicio41/src/ejercicio41/Ejercicio41.java
@@ -2,6 +2,9 @@ package ejercicio41;
 public class Ejercicio41 {
     public static void main(String[] args) {
         System.out.println("Ejercicio 41");
+        
+        System.out.println("Soy el alumno B.");
+        System.out.println("Paro para hacer commit.");
     }
     
 }
